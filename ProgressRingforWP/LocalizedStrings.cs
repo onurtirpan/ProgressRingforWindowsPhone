177d03aff76985775229ddda2f2c5c2103c63c40
onurtirpan <posta@onurtirpan.com>
2014-03-26 16:14:39 +0200

    Publish
    Publish

---
+++ ProgressRingforWP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ProgressRingforWP.Resources;
+
+namespace ProgressRingforWP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
